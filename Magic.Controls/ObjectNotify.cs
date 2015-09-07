83c96d3d544d75d5d16c30c51b84d3636b7d8af8
rolbeh <rolbeh@gmx.de>
2015-09-07 23:26:20 +0200

    Annotations and Base implementation of  INotifyPropertyChanged

---
+++ Magic.Controls/ObjectNotify.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Magic.Annotations;
+
+// ReSharper disable CheckNamespace
+namespace System
+{
+    /// <summary>
+    /// Base type for viewmodel and classes with standard implementation of <see cref="INotifyPropertyChanged"/>
+    /// </summary>
+    public class ObjectNotify : object, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
+// ReSharper restore CheckNamespace
